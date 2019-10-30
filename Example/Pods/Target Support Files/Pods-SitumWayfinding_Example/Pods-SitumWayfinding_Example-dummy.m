bc2273e209e868dbc6b0c9245f99b1b07b7a052e
fsvilas <fsvilas@situm.com>
2019-10-30 13:29:37 +0100

    Initial app commit

---
+++ Example/Pods/Target Support Files/Pods-SitumWayfinding_Example/Pods-SitumWayfinding_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SitumWayfinding_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SitumWayfinding_Example
+@end
---
+++ Example/Pods/Target Support Files/SitumWayfinding/SitumWayfinding-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SitumWayfinding : NSObject
+@end
+@implementation PodsDummy_SitumWayfinding
+@end
