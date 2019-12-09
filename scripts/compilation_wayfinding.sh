7e30cbecafd7c858ca4aad6d41a5f6344e17b688
Fernando Sanchez <fernando.sanchez@situm.es>
2019-12-09 18:00:50 +0100

    Verify app is generated in jenkins

--- scripts/compilation_wayfinding.sh
+++ scripts/compilation_wayfinding.sh
@@ -15,3 +15,12 @@ xcodebuild -workspace ./Example/SitumWayfinding.xcworkspace \
 -configuration $BUILD_TYPE \
 -derivedDataPath "build/derivedData" \
 -scheme SitumWayfinding-Example clean build 2>&1 | tee ./build/buildWayfinding.log
+
+# Change dir if debug release
+if [ "$BUILD_TYPE" == "Release" ]; then
+cd "build/derivedData/Build/Products/Release-iphoneos"
+else
+cd "build/derivedData/Build/Products/Debug-iphoneos"
+fi
+
+test -f "SitumWayfinding_Example.app"
