3633c68388fbce380b698abc818bda30cbb5222f
Abraham Barros <abraham.barros@situm.es>
2021-02-08 11:23:22 +0100

    using target

--- scripts/compilation_wayfinding.sh
+++ scripts/compilation_wayfinding.sh
@@ -14,7 +14,7 @@ fi
 xcodebuild -workspace ./Example/SitumWayfinding.xcworkspace \
 -configuration $BUILD_TYPE \
 -derivedDataPath "build/derivedData" \
--scheme SitumWayfinding-Example \
+-target SitumWayfinding-Example \
 PROVISIONING_PROFILE_SPECIFIER='$(APPLE_PROV_PROFILE_UUID)' CODE_SIGN_IDENTITY='$(APPLE_CERTIFICATE_SIGNING_IDENTITY)' clean build 2>&1 | tee ./build/buildWayfinding.log
 
 # Change dir if debug release
