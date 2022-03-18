a083f1d9c5078c2596ce96ec455826bae0190b10
brunodimensiona <bruno.gomez@dimensiona.com>
2022-03-18 12:16:08 +0100

    Change toast

--- SitumWayfinding/Classes/Helpers/Toast.swift
+++ SitumWayfinding/Classes/Helpers/Toast.swift
@@ -19,51 +19,5 @@ class Toast {
         view.transform = initialTransform
     }
     
-    func text(
-        title: String,
-        subtitle: String,
-        config: ToastConfiguration = ToastConfiguration()
-    ) -> Toast {
-        let view = GeneralToastView(child: TextToastView(title: title, subtitle: subtitle))
-        return self.init(view: view, config: config)
-    }
-    
-    func show(type: UINotificationFeedbackGenerator.FeedbackType, time: TimeInterval = 0) {
-        UINotificationFeedbackGenerator().notificationOccurred(type)
-        show(after: time)
-    }
-    
-    func show(after delay: TimeInterval = 0) {
-        config.view?.addSubview(view) ?? topController()?.view.addSubview(view)
-        view.createView(for: self)
-        
-        UIView.animate(withDuration: config.animationTime, delay: delay, options: [.curveEaseOut, .allowUserInteraction]) {
-            self.view.transform = .identity
-        } completion: { [self] _ in
-            if config.autoHide {
-                close(after: config.displayTime)
-            }
-        }
-    }
-    
-    func close(after time: TimeInterval = 0, completion: (() -> Void)? = nil) {
-        UIView.animate(withDuration: config.animationTime, delay: time, options: [.curveEaseIn, .allowUserInteraction], animations: {
-            self.view.transform = self.initialTransform
-        }, completion: { _ in
-            self.view.removeFromSuperview()
-            completion?()
-        })
-    }
-    
-    private func topController() -> UIViewController? {
-        let keyWindow = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
-        
-        if var topController = keyWindow?.rootViewController {
-            while let presentedViewController = topController.presentedViewController {
-                topController = presentedViewController
-            }
-            return topController
-        }
-        return nil
-    }
+  
 }
