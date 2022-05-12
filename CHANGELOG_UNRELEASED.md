d1c874fea1458ddced059848f070cbe5d742b02c
brunodimensiona <bruno.gomez@dimensiona.com>
2022-05-12 07:27:46 +0200

    Delete method public in LibrarySettings

--- CHANGELOG_UNRELEASED.md
+++ CHANGELOG_UNRELEASED.md
@@ -1,2 +1,3 @@
 ### Added:
-* Add new method in SitumMapsLibrary to limit zoom and pan to a single building
+* Add new method enableOneBuildingMode(buildingId: String) in SitumMapsLibrary to limit zoom and pan to a single building
+* Add new method enableOneBuildingMode(building: SITBuilding) in SitumMapsLibrary to limit zoom and pan to a single building
--- SitumWayfinding/Classes/WayfindingLibrary/LibrarySettings.swift
+++ SitumWayfinding/Classes/WayfindingLibrary/LibrarySettings.swift
@@ -41,8 +41,6 @@ import GoogleMaps
     /// Boolean that hides the back button in the navBar
     private (set) var showBackButton: Bool? = false
     
-    private (set) var modeOneBuilding: Bool? = false
-    
     // private(set) var orgDetails: OrganizationTheme?
     private override init() {
 
@@ -134,12 +132,6 @@ import GoogleMaps
             return self
         }
         
-        @discardableResult
-        @objc public func setModeOneBuilding(modeOneBuilding: Bool) -> Builder {
-            instance.modeOneBuilding = modeOneBuilding
-            return self
-        }
-
         /// Returns an instance of LibrarySettings
         @objc public func build() -> LibrarySettings {
             return instance
