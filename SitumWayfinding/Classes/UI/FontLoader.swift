525b1e96f672be70a761492caa82b69401462335
brunodimensiona <bruno.gomez@dimensiona.com>
2022-10-24 08:15:13 +0200

    Load dynamic fonts

--- SitumWayfinding/Classes/UI/FontLoader.swift
+++ SitumWayfinding/Classes/UI/FontLoader.swift
@@ -19,9 +19,17 @@ public class FontLoader {
         let urls = fonts.compactMap({ bundle.url(forResource: $0, withExtension: "ttf") })
         try urls.forEach { (url) in
             let data = try Data.init(contentsOf: url)
-            guard let provider = CGDataProvider.init(data: data as CFData) else { throw Error.error("CGDataProvider nil") }
-            guard let font = CGFont.init(provider) else { throw Error.error("CGFont nil") }
+            
+            guard let provider = CGDataProvider.init(data: data as CFData) else {
+                throw Error.error("CGDataProvider nil")
+            }
+            
+            guard let font = CGFont.init(provider) else {
+                throw Error.error("CGFont nil")
+            }
+            
             var error: Unmanaged<CFError>?
+            
             guard CTFontManagerRegisterGraphicsFont(font, &error) else {
                 throw error!.takeUnretainedValue()
             }
