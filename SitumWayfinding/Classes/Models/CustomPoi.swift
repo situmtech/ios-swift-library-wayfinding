7548865a96245d5cbb6477088c773585079459ed
Jose Alvarez <jose.alvarez@situm.com>
2023-02-20 17:59:11 +0100

    AppleDoc for public classes

--- SitumWayfinding/Classes/Models/CustomPoi.swift
+++ SitumWayfinding/Classes/Models/CustomPoi.swift
@@ -1,5 +1,5 @@
 //
-//  CustomMarkerPosition.swift
+//  CustomPoi.swift
 //  SitumWayfinding
 //
 //  Created by Jose Alvarez on 6/2/23.
@@ -7,12 +7,19 @@
 
 import Foundation
 
+/// Object that represents a custom POI saved by the user 
 public struct CustomPoi: Codable {
     let key: String
+    /// Name of the poi
     public let name: String?
+    /// Description of the poi
     public let description: String?
+    /// ID of the building where this POI is placed
     public let buildingId: String
+    /// ID of the floor where this POI is placed
     public let floorId: String
+    /// Latitude of the poi
     public let latitude: Double
+    /// Longitude of the poi
     public let longitude: Double
 }
